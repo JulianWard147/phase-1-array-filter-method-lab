ec82d2fe5bf3b717a8f498cef2007de6c2879179
Julian Ward <wardjulianm@gmail.com>
2023-02-16 11:22:25 -0700

    filters

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(driverArray, driverStringToMatch){
+    return driverArray.filter(input => input.toLowerCase() === driverStringToMatch.toLowerCase())
+  }
+  
+
+function fuzzyMatch(driverArray, driverStringToMatch){  
+    let stringLength = driverStringToMatch.length
+    return driverArray.filter(input => input.toLowerCase().slice(0, stringLength) === driverStringToMatch.toLowerCase())
+  }
+
+  
+
+function matchName(driverArrayofDicts, driverStringToMatch){
+    return driverArrayofDicts.filter(input => input.name === driverStringToMatch)
+  }
+  
